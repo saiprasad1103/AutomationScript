cbbde35ca7e4333218257d3c4095bfbcbd2df819
saiprasad.g <saiprasad.g@QT-INBG-LT0406.QUALITEST.LOCAL>
2022-08-02 14:00:35 +0530

    getting error

---
+++ automationscript/src/automationscript/Searchandorder.java
@@ -0,0 +1,70 @@
+package automationscript;
+
+import java.awt.Robot;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import 	org.testng.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import dev.failsafe.internal.util.Assert;
+import io.appium.java_client.android.nativekey.KeyEvent;
+
+public class Searchandorder {
+
+	public class Search extends Lanuch 
+	{
+	@Test
+	(priority = 2) 
+	public void search() throws InterruptedException 
+	{ 
+	driver.findElement(By.xpath("//a[.='All Mobiles']")).click(); 
+	WebElement ele = driver.findElement(By.xpath("//b[.='All Mobile Details']"));
+     Assert.assertTrue(ele.isDisplayed()); 
+	driver.findElement(By.id("myInput")).sendKeys("Apple Iphone 13"); 
+	Thread.sleep(2000); 
+	JavascriptExecutor j = (JavascriptExecutor) driver; 
+	j.executeScript("window.scrollBy(0,700)"); 
+	Assert.assertTrue(driver.findElement(By.xpath("//h2[.='Our New Feature Courses']")).isDisplayed()); 
+	driver.findElement(By.xpath("(//a[.='Order'])[4]")).click();
+	}
+	@Test(priority = 3) public void Order() throws InterruptedException, Exception
+	{ 
+	Set window = driver.getWindowHandles();
+	Iteratorit = window.iterator(); 
+	Thread.sleep(2000);
+	String parentId = it.next(); String childId = it.next();
+	driver.switchTo().window(childId);
+	Assert.assertTrue(driver.findElement(By.tagName("h2")).isDisplayed());
+	driver.findElement(By.id("inputFirstName")).sendKeys("soumya"); 
+	driver.findElement(By.xpath("(//*[@id='inputFirstName'])[2]")).sendKeys("kulkarni");
+	driver.findElement(By.id("inputEmail")).sendKeys("Sk@gmail.com");
+	driver.findElement(By.id("inputPassword")).sendKeys("Sou@123"); 
+	driver.findElement(By.id("flexRadioDefault2")).click(); 
+	driver.findElement(By.xpath("//*[@placeholder='00000000000']")).sendKeys("123456");
+	driver.findElement(By.id(" address1")).sendKeys("adcdef ghij kljhg jhun"); 
+	driver.findElement(By.id("address2")).sendKeys("abcd cvgb nkhb nhh nhbbb");
+	driver.findElement(By.id("inputCity")).sendKeys("Bangalore");
+	WebElement dropdown = driver.findElement(By.id("inputState")); 
+	Select sle= new Select(dropdown); sle.selectByVisibleText("Karnataka");
+	driver.findElement(By.id("inputZip")).sendKeys("560010"); 
+	driver.findElement(By.xpath("//*[.='Apple']")).click(); 
+	driver.findElement(By.cssSelector("input[placeholder='no of mobiles']")).sendKeys("3");
+	WebElement dpdwn2 = driver.findElement(By.id("bought"));
+	Select sle2= new Select(dpdwn2); sle2.selectByValue("0");
+	driver.findElement(By.xpath("//*[@id='gridCheck1']")).click(); 
+	driver.findElement(By.xpath("(//*[@id='gridCheck1'])[2]")).click();
+	driver.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
+	Robot ro=new Robot(); for(int i=0;i<2;i++) { ro.keyPress(KeyEvent.VK_TAB); 
+	}
+	ro.keyRelease(KeyEvent.VK_TAB); 
+	ro.keyPress(KeyEvent.VK_ENTER); 
+	ro.keyRelease(KeyEvent.VK_ENTER); 
+	Thread.sleep(2000);
+	} 
+	}
+
+}
