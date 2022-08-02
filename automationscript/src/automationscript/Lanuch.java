321d2f212e4a07ddf5b379d6469088f751a8f3a4
saiprasad.g <saiprasad.g@QT-INBG-LT0406.QUALITEST.LOCAL>
2022-08-02 10:59:03 +0530

    lanuch successfull

--- automationscript/src/automationscript/Lanuch.java
+++ automationscript/src/automationscript/Lanuch.java
@@ -3,15 +3,20 @@ package automationscript;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Lanuch {
 	
 
 	WebDriver driver; 
-	@BeforeMethod
+	@Test
 	public void Lanching_the_Browser() 
 	{
-	System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	
+	//System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	WebDriverManager.chromedriver().setup();
 	driver = new ChromeDriver(); 
 	driver.get("https://mobileworld.azurewebsites.net"); 
 	driver.manage().window().maximize(); 
